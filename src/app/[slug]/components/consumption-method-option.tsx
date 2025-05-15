91ee2fbe20be741f3eb00f82a8cec105611e716b
hiigorsilva <higorscontato@gmail.com>
2025-05-15 17:48:36 -0300

    feat: add consumption-method-option component

---
+++ src/app/[slug]/components/consumption-method-option.tsx
@@ -0,0 +1,45 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+import type { CONSUMPTION_METHOD } from '@prisma/client'
+import Image from 'next/image'
+import Link from 'next/link'
+
+type ConsumptionMethodProps = {
+  imageUrl: string
+  buttonText: string
+  imageAlt: string
+  consumptionOption: CONSUMPTION_METHOD
+}
+
+export const ConsumptionMethodOption = ({
+  consumptionOption,
+  buttonText,
+  imageUrl,
+  imageAlt,
+}: ConsumptionMethodProps) => {
+  return (
+    <Card className="p-0">
+      <CardContent className="p-6 space-y-8">
+        <div className="relative h-[80px] w-[80px] mx-auto">
+          <Image
+            className="object-contain"
+            src={imageUrl}
+            alt={imageAlt}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            fill
+          />
+        </div>
+
+        <Button
+          className="w-full font-semibold text-xs tracking-tight rounded-full"
+          variant="secondary"
+          asChild
+        >
+          <Link href={`menu?consumptionMethod=${consumptionOption}`}>
+            {buttonText}
+          </Link>
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
