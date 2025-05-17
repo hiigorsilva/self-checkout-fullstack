a6c6008b97e3d8419bfb5d1dade6aa24d34abbaf
hiigorsilva <higorscontato@gmail.com>
2025-05-17 03:00:49 -0300

    feat: add menu-categories in query find restaurant by slug

--- src/services/restaurant/getRestaurantBySlug.ts
+++ src/services/restaurant/getRestaurantBySlug.ts
@@ -3,6 +3,7 @@ import { db } from '@/lib/prisma'
 export const getRestaurantBySlug = async (slug: string) => {
   const restaurant = await db.restaurant.findUnique({
     where: { slug: slug },
+    include: { menuCategories: true },
   })
   return restaurant
 }
